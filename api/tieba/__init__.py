2b488d5fba5c43340f7c26353d866e37ffa591a5
283375 <bilibili283375@163.com>
2022-07-19 20:08:29 +0800

    Removing useless files

--- api/tieba/__init__.py
+++
@@ -1,2 +0,0 @@
-# All the methods are ported from https://github.com/HuanCheng65/TiebaLite
-# Great thanks to HuanCheng65.
--- api/tieba/generateSign.py
+++
@@ -1,7 +0,0 @@
-from hashlib import md5
-
-
-def generateSign(params: dict):
-    sortedParams = sorted(params.items())
-    sortedParamsStr = "".join(f"{k}={v}" for k, v in sortedParams) + "tiebaclient!!!"
-    return md5(sortedParamsStr.encode('utf-8')).hexdigest()
