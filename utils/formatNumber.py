02cd6138ee92e506125ec59db186292f4b8c64cc
283375 <bilibili283375@163.com>
2022-08-10 21:46:50 +0800

    fix: utils: formatNumber 默认单位处理

--- utils/formatNumber.py
+++ utils/formatNumber.py
@@ -6,23 +6,23 @@ def plain(val):
     return val
 
 
-def __generalFormat(val, size, units: list[str], formatStr: str = "{val}{unit}"):
+def __generalFormat(val, size, units: list[str], formatStr: str = "{val:.2f}{unit}"):
     __val = val
     __unit = ""
     for unit in units:
+        __unit = unit
         if __val < 950:
             break
         __val /= size
-        __unit = unit
     return formatStr.format(val=__val, unit=__unit)
 
 
 def bigNumber(val):
-    return __generalFormat(val, 1000, ["K", "M", "B"])
+    return __generalFormat(val, 1000, ["", "K", "M", "B"])
 
 
 def bigNumberChinese(val):
-    return __generalFormat(val, 10000, ["万", "亿"], "{val} {unit}")
+    return __generalFormat(val, 10000, ["", "万", "亿"], "{val:.2f} {unit}")
 
 
 def byte(val):
