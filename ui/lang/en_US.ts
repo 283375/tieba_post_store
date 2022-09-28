87b25a89b2a080e28e4680320459956ca3072f01
283375 <bilibili283375@163.com>
2022-09-29 00:06:45 +0800

    wip: ui: 语言文件试用

---
+++ ui/lang/en_US.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>ThreadInfo</name>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="48"/>
+        <source>versionLabel</source>
+        <translation>Store Version</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="49"/>
+        <source>idLabel</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="52"/>
+        <source>titleLabel</source>
+        <translation>Title</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="55"/>
+        <source>forumLabel</source>
+        <translation>Forum</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="58"/>
+        <source>authorLabel</source>
+        <translation>LZ (OP)</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="61"/>
+        <source>storeDirLabel</source>
+        <translation>Store Location</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="64"/>
+        <source>createTimeLabel</source>
+        <translation>Thread Posted</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="67"/>
+        <source>storeTimeLabel</source>
+        <translation>Stored At</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="70"/>
+        <source>updateTimeLabel</source>
+        <translation>Last Updated</translation>
+    </message>
+</context>
+<context>
+    <name>WorkDirectory</name>
+    <message>
+        <location filename="../base/WorkDirectory.py" line="32"/>
+        <source>changeDirButton</source>
+        <translation>Change</translation>
+    </message>
+</context>
+</TS>
---
+++ ui/lang/zh_CN.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ThreadInfo</name>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="48"/>
+        <source>versionLabel</source>
+        <translation>存档版本</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="49"/>
+        <source>idLabel</source>
+        <translation>贴子 ID</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="52"/>
+        <source>titleLabel</source>
+        <translation>标题</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="55"/>
+        <source>forumLabel</source>
+        <translatorcomment>实际显示：发表于 {forum}吧</translatorcomment>
+        <translation>发表于</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="58"/>
+        <source>authorLabel</source>
+        <translation>楼主</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="61"/>
+        <source>storeDirLabel</source>
+        <translatorcomment>实际显示：存档于 {storeDir} # /home/lh283375/store/...</translatorcomment>
+        <translation>存档于</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="64"/>
+        <source>createTimeLabel</source>
+        <translation>贴子发布时间</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="67"/>
+        <source>storeTimeLabel</source>
+        <translation>存档时间</translation>
+    </message>
+    <message>
+        <location filename="../base/ThreadInfo.py" line="70"/>
+        <source>updateTimeLabel</source>
+        <translation>最后更新时间</translation>
+    </message>
+</context>
+<context>
+    <name>WorkDirectory</name>
+    <message>
+        <location filename="../base/WorkDirectory.py" line="32"/>
+        <source>changeDirButton</source>
+        <translation>更改</translation>
+    </message>
+</context>
+</TS>
